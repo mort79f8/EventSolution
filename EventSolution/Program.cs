3b99e3f432d664610a45aab8e84d9615cdc713bb
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-02-27 13:40:59 +0100

    Add project files.

---
+++ EventSolution/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSolution
+{
+    class Program
+    {
+
+        public delegate void myEventHandler(int value);
+
+        class PiggyBankEventHandler
+        {
+            private int total;
+
+            // declare the event
+            public event myEventHandler valueChanged;
+
+            public int Total
+            {
+                get => total;
+                set
+                {
+                    total = value;
+                    // when the value changes, fire the event
+                    valueChanged(total);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PiggyBankEventHandler piggybank = new PiggyBankEventHandler();
+            piggybank.valueChanged += Piggybank_ValueChanged;
+
+            string str;
+            do
+            {                                
+                Console.WriteLine("Enter the amount you which to put in the bank:");
+                str = Console.ReadLine();
+                if (!str.Equals("exit"))
+                {
+                    piggybank.Total += int.Parse(str);
+                }
+            } while (!str.Equals("exit"));
+
+        }
+
+        static void Piggybank_ValueChanged(int value)
+        {
+            int savingsGoal = 500;
+            if (value >= savingsGoal)
+            {
+                Console.WriteLine("You have reach your savings goal!!");
+            }
+
+            Console.WriteLine($"Total in the piggybank: {value}");
+        }
+    }
+}
